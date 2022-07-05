ed3b9615557f32c96ccd0d41e8f2f7f8b7e80a4a
ingemar-andre <ingemar.andre@biochemistry.lu.se>
2022-07-05 12:32:24 +0200

    updating documentation

--- production_run/run_production.sh
+++ production_run/run_production.sh
@@ -1,2 +1,2 @@
 #Example for running a production evolution run
-rosetta_scripts.default.linuxgccrelease -s in/5azu.pdb -parser:protocol equilibrate_sequence.xml @flags -overwrite -out:prefix pdbs/5azu. -parser:script_vars id=5azu e_delta=-427.566 offset=-427.566 n_trials=127
+rosetta_scripts.default.linuxgccrelease -s in/5azu.pdb -parser:protocol production_run.xml @flags -overwrite -out:prefix pdbs/5azu. -parser:script_vars id=5azu e_delta=-427.566 offset=-427.566 n_trials=127
